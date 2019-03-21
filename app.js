e5a6c840169deadd92f2d6d79f038bcec1ad9281
John Pickett <john.pickett@maryville.com>
2019-03-21 15:53:25 -0500

    Put, and Delete endpoints added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const seedData = require('./db/seedDB');
 
 app.use(bodyParser.json());
 
-const eraseDatabaseOnSync = true;
+const eraseDatabaseOnSync = false;
 
 sequelize.sync({force: eraseDatabaseOnSync}).then(() => {
     if (eraseDatabaseOnSync) {
@@ -32,11 +32,135 @@ app.get('/', (req, res) => {
 app.get('/dogs', (req, res) => {
 	Dog.findAll().then((dogs) => {
 		res.send(dogs)
+	});
+});
+
+app.get('/dogs/:id', (req, res) => {
+	const id = req.params.id;
+	Dog.findOne({
+		where: {
+			id: id
+		}
+	}).then((dog) => {
+		if (!dog) {
+			res.status(404)
+		}
+		res.send(JSON.stringify(dog))
+	}).catch((err) => {
+		res.status(400)
+		res.send(err)
+	});
+});
+
+app.post('/dogs', (req, res) => {
+	const name = req.body.name;
+	const breed = req.body.breed;
+
+	const newDog = Dog.build({
+		name: name,
+		breed: breed
+	});
+
+	newDog.save().then((dog) => {
+		res.send('Dog saved: ' + JSON.stringify(dog))
+	})
+});
+
+app.put('/dogs/:id', (req, res) => {
+	const id = req.params.id;
+	const name = req.body.name;
+	const breed = req.body.breed;
+
+	Dog.update({
+		name: name,
+		breed: breed
+	}, { 
+		returning: true,
+		where: { id }
+	}).then((dog) => {
+		res.send(JSON.stringify(dog[1]))
+	}).catch((err) => {
+		res.status(400)
+		res.send(err)
+	});
+});
+
+app.delete('/dogs/:id', (req, res) => {
+	const id = req.params.id;
+
+	Dog.findOne({
+		where: { id }
+	}).then((dog) => {
+		dog.destroy().then((record) => {
+			res.send('Record id: ' + id + ' was deleted.')
+		})
 	})
 });
 
 app.get('/cats', (req, res) => {
 	Cat.findAll().then((cats) => {
 		res.send(cats)
+	});
+});
+
+app.get('/cats/:id', (req, res) => {
+	const id = req.params.id;
+	Cat.findOne({
+		where: {
+			id: id
+		}
+	}).then((cat) => {
+		if (!cat) {
+			res.status(404)
+		}
+		res.send(JSON.stringify(cat))
+	}).catch((err) => {
+		res.status(400)
+		res.send(err)
+	});
+});
+
+app.post('/cats', (req, res) => {
+	const name = req.body.name;
+	const breed = req.body.breed;
+
+	const newCat = Cat.build({
+		name: name,
+		breed: breed
+	});
+
+	newCat.save().then((cat) => {
+		res.send('Cat saved: ' + JSON.stringify(cat))
+	})
+});
+
+app.put('/cats/:id', (req, res) => {
+	const id = req.params.id;
+	const name = req.body.name;
+	const breed = req.body.breed;
+
+	Cat.update({
+		name: name,
+		breed: breed
+	}, { 
+		returning: true,
+		where: { id }
+	}).then((cat) => {
+		res.send(JSON.stringify(cat[1]))
+	}).catch((err) => {
+		res.status(400)
+		res.send(err)
+	});
+});
+
+app.delete('/cats/:id', (req, res) => {
+	const id = req.params.id;
+
+	Cat.findOne({
+		where: { id }
+	}).then((cat) => {
+		cat.destroy().then((record) => {
+			res.send('Record id: ' + id + ' was deleted.')
+		})
 	})
 });
